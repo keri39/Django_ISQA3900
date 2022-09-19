7e719b2101d947a4078866314863b6fcfba57904
Abhishek Keri <111935093+keri39@users.noreply.github.com>
2022-09-19 13:45:23 -0500

    Update settings.py

--- localLibrary/settings.py
+++ localLibrary/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from django.conf.global_settings import EMAIL_USE_TLS, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
